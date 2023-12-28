4c8506beb47e3150ede84b0a3fa6f3a3fd69a153
genesis12 <togarisbless@gmail.com>
2023-12-29 02:39:37 +0530

    Create Auth for User and Admin

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class AdminController extends Controller {
+	public function AdminDashboard() {
+
+		return view('admin.admin_dashboard');
+
+	} // End Method
+}
---
+++ resources/views/admin/admin_dashboard.blade.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+
+	<title>
+		ADMIN DASHBOARD
+	</title>
+
+</head>
+
+
+<body>
+
+<div>
+
+	<h1>
+		ADMIN DASHBOARD
+	</h1>
+</div>
+
+
+
+</body>
+
+
+
+</html>
