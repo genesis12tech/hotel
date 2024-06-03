c01cdb00acb9f61aa6d5cb005d8fbf8484bdb5ea
thomas <togarisbless@gmail.com>
2024-06-03 18:27:16 +0530

    Update Admin Dashboard

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,7 +1,18 @@
 @extends('admin.admin_dashboard')
 @section('admin')
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
+@php
+  $bookings = App\Models\Booking::latest()->get();
+  $pending = App\Models\Booking::where('status','0')->get();
+  $complete = App\Models\Booking::where('status','1')->get();
+  $totalPrice = App\Models\Booking::sum('total_price');
+  $today = Carbon\Carbon::now()->toDateString();
+  $todayprice = App\Models\Booking::whereDate('created_at',$today)->sum('total_price');
+  $allData = App\Models\Booking::orderBy('id','desc')->limit(10)->get();
+@endphp
 
 
 
@@ -12,9 +23,9 @@
 						<div class="card-body">
 							<div class="d-flex align-items-center">
 								<div>
-									<p class="mb-0 text-secondary">Total Orders</p>
-									<h4 class="my-1 text-info">4805</h4>
-									<p class="mb-0 font-13">+2.5% from last week</p>
+									<p class="mb-0 text-secondary">Total Booking</p>
+                        <h4 class="my-1 text-info">{{ count($bookings) }}</h4>
+                        <p class="mb-0 font-13">Today Sale:  ${{ $todayprice }}</p>
 								</div>
 								<div class="widgets-icons-2 rounded-circle bg-gradient-blues text-white ms-auto"><i class='bx bxs-cart'></i>
 								</div>
@@ -27,8 +38,8 @@
 					   <div class="card-body">
 						   <div class="d-flex align-items-center">
 							   <div>
-								   <p class="mb-0 text-secondary">Total Revenue</p>
-								   <h4 class="my-1 text-danger">$84,245</h4>
+								<p class="mb-0 text-secondary">Pening Booking</p>
+								<h4 class="my-1 text-danger">{{ count($pending) }}</h4>
 								   <p class="mb-0 font-13">+5.4% from last week</p>
 							   </div>
 							   <div class="widgets-icons-2 rounded-circle bg-gradient-burning text-white ms-auto"><i class='bx bxs-wallet'></i>
@@ -42,8 +53,8 @@
 					   <div class="card-body">
 						   <div class="d-flex align-items-center">
 							   <div>
-								   <p class="mb-0 text-secondary">Bounce Rate</p>
-								   <h4 class="my-1 text-success">34.6%</h4>
+								<p class="mb-0 text-secondary">Complete Booking</p>
+								<h4 class="my-1 text-success">{{ count($complete) }}</h4>
 								   <p class="mb-0 font-13">-4.5% from last week</p>
 							   </div>
 							   <div class="widgets-icons-2 rounded-circle bg-gradient-ohhappiness text-white ms-auto"><i class='bx bxs-bar-chart-alt-2' ></i>
@@ -57,8 +68,8 @@
 					   <div class="card-body">
 						   <div class="d-flex align-items-center">
 							   <div>
-								   <p class="mb-0 text-secondary">Total Customers</p>
-								   <h4 class="my-1 text-warning">8.4K</h4>
+								<p class="mb-0 text-secondary">Total Price</p>
+                       <h4 class="my-1 text-warning">${{ $totalPrice  }}</h4>
 								   <p class="mb-0 font-13">+8.4% from last week</p>
 							   </div>
 							   <div class="widgets-icons-2 rounded-circle bg-gradient-orange text-white ms-auto"><i class='bx bxs-group'></i>
@@ -68,6 +79,10 @@
 					</div>
 				  </div>
 				</div><!--end row-->
+
+
+
+
 
 				<div class="row">
        <div class="col-12 col-lg-12 d-flex">
@@ -83,7 +98,7 @@
 
               <div class="row row-cols-1 row-cols-md-3 row-cols-xl-3 g-0 row-group text-center border-top">
 
-               <canvas id="Chart1"></canvas>
+               <canvas id="bookingChart"></canvas>
 
               </div>
           </div>
@@ -98,106 +113,86 @@
 					<div class="card-header">
 						<div class="d-flex align-items-center">
 							<div>
-								<h6 class="mb-0">Recent Orders</h6>
+								<h6 class="mb-0">Recent Booking</h6>
 							</div>
 
 						</div>
 					</div>
-                         <div class="card-body">
-						 <div class="table-responsive">
-						   <table class="table align-middle mb-0">
-							<thead class="table-light">
-							 <tr>
-							   <th>Product</th>
-							   <th>Photo</th>
-							   <th>Product ID</th>
-							   <th>Status</th>
-							   <th>Amount</th>
-							   <th>Date</th>
-							   <th>Shipping</th>
-							 </tr>
-							 </thead>
-							 <tbody><tr>
-							  <td>Iphone 5</td>
-							  <td><img src="assets/images/products/01.png" class="product-img-2" alt="product img"></td>
-							  <td>#9405822</td>
-							  <td><span class="badge bg-gradient-quepal text-white shadow-sm w-100">Paid</span></td>
-							  <td>$1250.00</td>
-							  <td>03 Feb 2020</td>
-							  <td><div class="progress" style="height: 6px;">
-									<div class="progress-bar bg-gradient-quepal" role="progressbar" style="width: 100%"></div>
-								  </div></td>
-							 </tr>
-
-							 <tr>
-							  <td>Earphone GL</td>
-							  <td><img src="assets/images/products/02.png" class="product-img-2" alt="product img"></td>
-							  <td>#8304620</td>
-							  <td><span class="badge bg-gradient-blooker text-white shadow-sm w-100">Pending</span></td>
-							  <td>$1500.00</td>
-							  <td>05 Feb 2020</td>
-							  <td><div class="progress" style="height: 6px;">
-									<div class="progress-bar bg-gradient-blooker" role="progressbar" style="width: 60%"></div>
-								  </div></td>
-							 </tr>
-
-							 <tr>
-							  <td>HD Hand Camera</td>
-							  <td><img src="assets/images/products/03.png" class="product-img-2" alt="product img"></td>
-							  <td>#4736890</td>
-							  <td><span class="badge bg-gradient-bloody text-white shadow-sm w-100">Failed</span></td>
-							  <td>$1400.00</td>
-							  <td>06 Feb 2020</td>
-							  <td><div class="progress" style="height: 6px;">
-									<div class="progress-bar bg-gradient-bloody" role="progressbar" style="width: 70%"></div>
-								  </div></td>
-							 </tr>
-
-							 <tr>
-							  <td>Clasic Shoes</td>
-							  <td><img src="assets/images/products/04.png" class="product-img-2" alt="product img"></td>
-							  <td>#8543765</td>
-							  <td><span class="badge bg-gradient-quepal text-white shadow-sm w-100">Paid</span></td>
-							  <td>$1200.00</td>
-							  <td>14 Feb 2020</td>
-							  <td><div class="progress" style="height: 6px;">
-									<div class="progress-bar bg-gradient-quepal" role="progressbar" style="width: 100%"></div>
-								  </div></td>
-							 </tr>
-							 <tr>
-							  <td>Sitting Chair</td>
-							  <td><img src="assets/images/products/06.png" class="product-img-2" alt="product img"></td>
-							  <td>#9629240</td>
-							  <td><span class="badge bg-gradient-blooker text-white shadow-sm w-100">Pending</span></td>
-							  <td>$1500.00</td>
-							  <td>18 Feb 2020</td>
-							  <td><div class="progress" style="height: 6px;">
-									<div class="progress-bar bg-gradient-blooker" role="progressbar" style="width: 60%"></div>
-								  </div></td>
-							 </tr>
-							 <tr>
-							  <td>Hand Watch</td>
-							  <td><img src="assets/images/products/05.png" class="product-img-2" alt="product img"></td>
-							  <td>#8506790</td>
-							  <td><span class="badge bg-gradient-bloody text-white shadow-sm w-100">Failed</span></td>
-							  <td>$1800.00</td>
-							  <td>21 Feb 2020</td>
-							  <td><div class="progress" style="height: 6px;">
-									<div class="progress-bar bg-gradient-bloody" role="progressbar" style="width: 40%"></div>
-								  </div></td>
-							 </tr>
-						    </tbody>
-						  </table>
-						  </div>
-						 </div>
+					<div class="card-body">
+						<div class="table-responsive">
+							<table class="table table-striped table-bordered" style="width:100%">
+								<thead>
+									<tr>
+										<th>Sl</th>
+										<th>B No</th>
+										<th>B Date</th>
+										<th>Customer</th>
+										<th>Room</th>
+										<th>Check IN/Out</th>
+										<th>Total Room</th>
+										<th>Guest</th> 
+									</tr>
+								</thead>
+								<tbody>
+								   @foreach ($allData as $key=> $item ) 
+									<tr>
+										<td>{{ $key+1 }}</td>
+										<td> <a href="{{ route('edit_booking',$item->id) }}"> {{ $item->code }} </a></td>
+										<td> {{ $item->created_at->format('d/m/Y') }} </td>
+										<td> {{ $item['user']['name'] }} </td>
+										<td> {{ $item['room']['type']['name'] }} </td>
+										<td> <span class="badge bg-primary">{{ $item->check_in }}</span>   <span class="badge bg-warning text-dark">{{ $item->check_out }}</span> </td>
+										<td> {{ $item->number_of_rooms }} </td>
+										<td> {{ $item->persion }} </td>
+			  
+			  
+									</tr>
+									@endforeach 
+			  
+								</tbody>
+			  
+							</table>
+						</div>
+					</div>
 					</div>
 
 
-
-
-
-
 			</div>
+
+
+
+			<script>
+				var ctx = document.getElementById('bookingChart').getContext('2d');
+				var bookings = @json($bookings);
+
+				// Extract the required data from the bookings
+				var labels = bookings.map(function(booking) {
+					return booking.check_in; 
+				});
+				var data = bookings.map(function(booking) {
+					return booking.total_price;
+				});
+				var bookingChart = new Chart(ctx, {
+					type: 'bar',
+					data: {
+						labels: labels,
+						datasets: [{
+							label: 'Booking Data',
+							data: data,
+							backgroundColor: 'rgba(75, 192, 192, 0.2)',
+							borderColor: 'rgba(75, 192, 192, 1)',
+							borderWidth: 1
+						}]
+					},
+					options: {
+						scales: {
+							y: {
+								beginAtZero: true
+							}
+						}
+					}
+				});
+			  </script>
 
 
 
